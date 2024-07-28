70b30a4e9f51d9ee732d8ee8ab696c14b9a2d4c7
deus-tll <millerartemka14@gmail.com>
2024-07-28 19:54:39 +0300

    Revert "Merge branch 'dev' into b-miller"
    This reverts commit 70a993094992ddd9024826539c68646f89e431e3, reversing
    changes made to de17bfbad4785a16bf89fb865041d8e57eec30cf.

--- backend/auth/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/config.json').development;
-
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-});
-
-module.exports = sequelize;
