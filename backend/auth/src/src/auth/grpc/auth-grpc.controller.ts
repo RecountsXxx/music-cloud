7ee23c937bfc15b0d8f4280feb21d661c41a3c8b
deus-tll <millerartemka14@gmail.com>
2024-07-29 21:02:08 +0300

    Update auth-grpc.controller.ts

--- backend/auth/src/src/auth/grpc/auth-grpc.controller.ts
+++ backend/auth/src/src/auth/grpc/auth-grpc.controller.ts
@@ -1,19 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { AuthService } from '../auth.service';
+import { UserDto } from '../dtos/user.dto';
 
 @Controller()
 export class AuthGrpcController {
   constructor(private readonly authService: AuthService) {}
 
   @GrpcMethod('AuthService', 'ValidateToken')
-  async validateToken(data: { token: string }) {
+  async validateToken(data: { token: string }) : Promise<{ valid: boolean; user: UserDto | null }> {
     const { valid, user } = await this.authService.validateToken(data.token);
     return {
       valid,
-      user: user
-        ? { id: user.id, email: user.email, username: user.username }
-        : null,
+      user
     };
   }
 }
