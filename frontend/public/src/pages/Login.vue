4a4ce82ee45e2707bd8af6678b9ac7dcdc280d6e
Administrator <dankoshevoy2001@gmail.com>
2024-07-30 01:53:08 +0300

    update

--- frontend/public/src/pages/Login.vue
+++ frontend/public/src/pages/Login.vue
@@ -1,40 +1,58 @@
 <template>
-  <main class="main-content">
-    <section class="login-container">
-      <h1 class="login__title">{{ $t('loginForm.form__title') }}</h1>
-      <form class="login__form" @submit.prevent="handleSubmit">
-        <div class="login__field">
-          <label class="login-form__label" for="email"></label>
-          <input class="login-form__input" v-model="email" id="email" type="text" required
-                 :placeholder="$t('loginForm.placeholder.email')">
-        </div>
-        <div class="login_form__field">
-          <label class="login-form__label" for="password"></label>
-          <div class="password__wrapper">
-            <input class="login-form__input" v-model="password" id="password"
-                   type="password" required
-                   ref="passwordInput"
-                   :placeholder="$t('loginForm.placeholder.password')">
-            <input id="showPassword" type="button" :value="passShowLabel" @click="changeVisiblePassword">
-          </div>
-        </div>
-        <div class="remember-me">
-          <input id="remember-me" name="remember-me" type="checkbox" v-model="rememberMe">
-          <label for="remember-me">{{ $t('loginForm.remember-me') }}</label>
-        </div>
-        <div class="forgot-password">
-          <router-link to="#">{{ $t('loginForm.forgot-password') }}</router-link>
-        </div>
-        <input type="submit" class="login-form_button" :value="$t('loginForm.buttonSubmit')">
-      </form>
-    </section>
-    <div class="register">
-      <p>{{ $t('register.noRegister') }}
-        <router-link to="#">{{ $t('register.createAccount') }}</router-link>
-      </p>
-    </div>
+  <main id="main__container">
+    <div class="login__title">{{ $t('loginForm.form__title') }}</div>
+    <form id="login__form" @submit.prevent="handleSubmit">
+      <input class="form__input" v-model="email" id="email" type="text" required
+             :placeholder="$t('loginForm.placeholder.email')">
+
+      <div class="password__field">
+        <input class="form__input" v-model="password" id="password"
+               type="password" required
+               ref="passwordInput"
+               :placeholder="$t('loginForm.placeholder.password')">
+        <img class="showHidePassword" src="../images/showPassword.svg" alt="">
+      </div>
+    </form>
   </main>
 </template>
+
+<!--<template>-->
+<!--  <main class="main-content">-->
+<!--    <section class="login-container">-->
+<!--      <h1 class="login__title">{{ $t('loginForm.form__title') }}</h1>-->
+<!--      <form class="login__form" @submit.prevent="handleSubmit">-->
+<!--        <div class="login__field">-->
+<!--          <label class="login-form__label" for="email"></label>-->
+<!--          <input class="login-form__input" v-model="email" id="email" type="text" required-->
+<!--                 :placeholder="$t('loginForm.placeholder.email')">-->
+<!--        </div>-->
+<!--        <div class="login_form__field">-->
+<!--          <label class="login-form__label" for="password"></label>-->
+<!--          <div class="password__wrapper">-->
+<!--            <input class="login-form__input" v-model="password" id="password"-->
+<!--                   type="password" required-->
+<!--                   ref="passwordInput"-->
+<!--                   :placeholder="$t('loginForm.placeholder.password')">-->
+<!--            <input id="showPassword" type="button" :value="passShowLabel" @click="changeVisiblePassword">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="remember-me">-->
+<!--          <input id="remember-me" name="remember-me" type="checkbox" v-model="rememberMe">-->
+<!--          <label for="remember-me">{{ $t('loginForm.remember-me') }}</label>-->
+<!--        </div>-->
+<!--        <div class="forgot-password">-->
+<!--          <router-link to="#">{{ $t('loginForm.forgot-password') }}</router-link>-->
+<!--        </div>-->
+<!--        <input type="submit" class="login-form_button" :value="$t('loginForm.buttonSubmit')">-->
+<!--      </form>-->
+<!--    </section>-->
+<!--    <div class="register">-->
+<!--      <p>{{ $t('register.noRegister') }}-->
+<!--        <router-link to="#">{{ $t('register.createAccount') }}</router-link>-->
+<!--      </p>-->
+<!--    </div>-->
+<!--  </main>-->
+<!--</template>-->
 
 <script>
 import {showHidePassword} from "../composables/showHidePassword";
