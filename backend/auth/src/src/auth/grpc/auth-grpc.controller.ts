deff9ddcc59f3214d0e7612435221a0d8ea379c7
deus-tll <millerartemka14@gmail.com>
2024-08-01 21:02:09 +0300

    updating grpc server in auth microservice

--- backend/auth/src/src/auth/grpc/auth-grpc.controller.ts
+++ backend/auth/src/src/auth/grpc/auth-grpc.controller.ts
@@ -3,16 +3,16 @@ import { GrpcMethod } from '@nestjs/microservices';
 import { AuthService } from '../auth.service';
 import { UserDto } from '../dtos/user.dto';
 
-@Controller()
+@Controller('grpc-auth')
 export class AuthGrpcController {
   constructor(private readonly authService: AuthService) {}
 
   @GrpcMethod('AuthService', 'ValidateToken')
-  async validateToken(data: { token: string }) : Promise<{ valid: boolean; user: UserDto | null }> {
-    const { valid, user } = await this.authService.validateToken(data.token);
+  async validateToken(data: { token: string }) : Promise<{ valid: boolean; userDTO: UserDto | null }> {
+    const { valid, userDTO } = await this.authService.validateToken(data.token);
     return {
       valid,
-      user
+      userDTO
     };
   }
 }
--- backend/auth/src/src/auth/grpc/grpc-options.ts
+++ backend/auth/src/src/auth/grpc/grpc-options.ts
@@ -4,7 +4,8 @@ import { Transport, ClientOptions } from '@nestjs/microservices';
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
+    url: 'auth.api:3001',
     package: 'auth',
-    protoPath: join(__dirname, 'auth.proto'),
+    protoPath: join(__dirname, 'auth.proto')
   }
 };
