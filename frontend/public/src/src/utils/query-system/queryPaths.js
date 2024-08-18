947df879fbea79947a7259754c3bdac1c5c93b88
deus-tll <millerartemka14@gmail.com>
2024-08-18 14:34:58 +0300

    update

--- frontend/public/src/src/utils/query-system/queryPaths.js
+++ frontend/public/src/src/utils/query-system/queryPaths.js
@@ -1,4 +1,4 @@
-export const QueryActions = {
+export const QueryPaths = {
   register: () => { return "/auth/register" },
   login: () => { return `/auth/login` },
   requestFileId: () => { return `/java/protected/upload/request-file-id` },
