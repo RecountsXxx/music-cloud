ec0410a3025d7b30bdab82dab7a6500a80be57f4
deus-tll <millerartemka14@gmail.com>
2024-09-12 19:40:22 +0300

    reorganizing project

--- backend/auth/src/src/grpc/auth/auth-grpc-options.ts
+++ backend/auth/src/src/grpc/auth/auth-grpc-options.ts
@@ -4,7 +4,7 @@ import { Transport, ClientOptions } from '@nestjs/microservices';
 export const authGrpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
-    url: 'auth.api:3010',
+    url: 'api.auth:3010',
     package: 'auth',
     protoPath: join(__dirname, 'auth.proto'),
   },
