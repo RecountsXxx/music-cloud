3e179aa0b65f2703e9d97d617a7f3f3a3e75bd5a
deus-tll <millerartemka14@gmail.com>
2024-10-05 18:58:58 +0300

    minor changes to api upload

--- backend/java/api-upload/src/src/main/java/org/deus/src/services/collection/CoverService.java
+++ backend/java/api-upload/src/src/main/java/org/deus/src/services/collection/CoverService.java
@@ -26,6 +26,7 @@ public class CoverService {
 
     public ResponseEntity<SuccessMessageResponse> coverUpload(String collectionId, String userId, MultipartFile cover) throws StatusException {
         String queueName = "convert.cover";
+        String messageError = "";
 
         try {
             this.storageCoverService.putOriginalBytes(collectionId, cover.getBytes());
@@ -34,19 +35,20 @@ public class CoverService {
 
             return ResponseEntity.ok(new SuccessMessageResponse("Cover upload process initiated. Please wait..."));
         }
-        catch (IOException | DataSavingException | MessageSendingException e) {
-            String message = "Failed to upload cover file!" + e.getMessage();
-            logger.error(message, e);
+        catch (IOException e) {
+            messageError = "Failed to upload cover file!" + e.getMessage();
+            logger.error(messageError, e);
 
-            if (e instanceof IOException) {
-                throw new StatusException("An error occurred while processing the uploaded cover file.", HttpStatus.INTERNAL_SERVER_ERROR);
-            } else if (e instanceof DataSavingException) {
-                throw new StatusException("Failed to save the cover data.", HttpStatus.INTERNAL_SERVER_ERROR);
-            } else if (e instanceof MessageSendingException) {
-                throw new StatusException("Failed to send the CoverConvertingDTO to the message queue \"" + queueName + "\".", HttpStatus.INTERNAL_SERVER_ERROR);
-            } else {
-                throw new StatusException(message, HttpStatus.INTERNAL_SERVER_ERROR); // Default for unknown errors
-            }
+            throw new StatusException("An error occurred while processing the uploaded cover file.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        catch (DataSavingException e) {
+            throw new StatusException("Failed to save the cover data.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        catch (MessageSendingException e) {
+            throw new StatusException("Failed to send the CoverConvertingDTO to the message queue \"" + queueName + "\".", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        catch (Exception e) {
+            throw new StatusException(messageError, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
