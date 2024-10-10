080b555b601bba8ff22dc3f8f86ec33f33092e76
Administrator <dankoshevoy2001@gmail.com>
2024-10-10 06:07:49 +0300

    up

--- frontend/public/src/src/pages/Preview.vue
+++ frontend/public/src/src/pages/Preview.vue
@@ -2,11 +2,13 @@
   <div class="main-container">
     <div class="main-content">
       <div class="m-content">
-        <Header/>
+        <Header />
         <div class="preview-title">{{ $t('PreviewPage.title') }}</div>
         <div class="preview-text">{{ $t('PreviewPage.text') }}</div>
         <div class="preview-info">{{ $t('PreviewPage.info') }}</div>
-        <router-link class="link-preview" :to="{ name: 'Register' }">{{ $t('PreviewPage.linkRegister') }}</router-link>
+        <router-link class="link-preview" :to="{ name: 'Register' }"
+          >{{ $t('PreviewPage.linkRegister') }}
+        </router-link>
       </div>
     </div>
     <div class="content-wrapper">
@@ -16,16 +18,16 @@
           <div class="text">{{ $t('PreviewPage.Recommended.text') }}</div>
           <div class="avatars">
             <div class="avatar">
-              <img src="@/assets/images/preview/recommended/img1.jpg" alt="Avatar 1">
+              <img src="@/assets/images/preview/recommended/img1.jpg" alt="Avatar 1" />
             </div>
             <div class="avatar">
-              <img src="@/assets/images/preview/recommended/img2.jpg" alt="Avatar 2">
+              <img src="@/assets/images/preview/recommended/img2.jpg" alt="Avatar 2" />
             </div>
             <div class="avatar">
-              <img src="@/assets/images/preview/recommended/img3.jpg" alt="Avatar 3">
+              <img src="@/assets/images/preview/recommended/img3.jpg" alt="Avatar 3" />
             </div>
             <div class="avatar">
-              <img src="@/assets/images/preview/recommended/img4.jpg" alt="Avatar 4">
+              <img src="@/assets/images/preview/recommended/img4.jpg" alt="Avatar 4" />
             </div>
           </div>
         </div>
@@ -35,18 +37,20 @@
           <div class="info">
             <div class="title">{{ $t('PreviewPage.Playlists.title') }}</div>
             <div class="text">{{ $t('PreviewPage.Playlists.text') }}</div>
-            <router-link :to="{ name: 'Register' }">{{ $t('PreviewPage.Playlists.buttonCreate') }}</router-link>
+            <router-link :to="{ name: 'Register' }"
+              >{{ $t('PreviewPage.Playlists.buttonCreate') }}
+            </router-link>
           </div>
 
           <div class="playlists">
             <div class="playlist">
-              <img src="@/assets/images/preview/playlists/hit1.jpg" alt="Piano Meditation">
+              <img src="@/assets/images/preview/playlists/hit1.jpg" alt="Piano Meditation" />
             </div>
             <div class="playlist">
-              <img src="@/assets/images/preview/playlists/hit2.jpg" alt="Hot Summer Hits">
+              <img src="@/assets/images/preview/playlists/hit2.jpg" alt="Hot Summer Hits" />
             </div>
             <div class="playlist">
-              <img src="@/assets/images/preview/playlists/hit3.jpg" alt="Dance Hits">
+              <img src="@/assets/images/preview/playlists/hit3.jpg" alt="Dance Hits" />
             </div>
           </div>
         </div>
@@ -57,7 +61,7 @@
 </template>
 
 <script setup>
-import Header from "@/components/header/Header.vue";
+import Header from '@/components/header/Header.vue'
 </script>
 
 <style lang="scss">
