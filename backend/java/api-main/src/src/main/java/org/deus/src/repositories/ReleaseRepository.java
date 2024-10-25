137806e16c050a4560d492752f4a59aab32862ac
deus-tll <millerartemka14@gmail.com>
2024-10-25 21:31:31 +0300

    updating certain repositories

--- backend/java/api-main/src/src/main/java/org/deus/src/repositories/ReleaseRepository.java
+++ backend/java/api-main/src/src/main/java/org/deus/src/repositories/ReleaseRepository.java
@@ -2,11 +2,17 @@ package org.deus.src.repositories;
 
 import org.deus.src.models.ReleaseModel;
 import org.deus.src.models.UserProfileModel;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReleaseRepository extends JpaRepository<ReleaseModel, UUID> {
+    Optional<ReleaseModel> findByIdAndCreatorUserProfile(UUID id, UserProfileModel creatorUserProfile);
     List<ReleaseModel> findAllByCreatorUserProfile(UserProfileModel creatorUserProfile);
+    @Query("SELECT r FROM ReleaseModel r ORDER BY r.numberOfLikes DESC")
+    List<ReleaseModel> findTopReleases(Pageable pageable);
 }
