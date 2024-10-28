431efc5da8a580667d7745474c86587fceebf4df
Renzo-Dev <dankoshevoy2001@gmail.com>
2024-10-28 17:55:07 +0300

    up

--- frontend/public/src/src/components/Main/genreCategory/GenreCategory.vue
+++ frontend/public/src/src/components/Main/genreCategory/GenreCategory.vue
@@ -1,83 +1,36 @@
-<!-- Жанры и категории -->
-<section class="genres wave">
-<div class="genres__title">{{ $t('PreviewPage.genres.title') }}</div>
-<div class="genres__description">{{ $t('PreviewPage.genres.description') }}</div>
-<div class="genres__list">
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/hiphop.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">{{ $t('PreviewPage.genres.items.hiphop') }}</div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Country.png" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">{{ $t('PreviewPage.genres.items.country') }}</div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Disco.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">{{ $t('PreviewPage.genres.items.disco') }}</div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Rock.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">{{ $t('PreviewPage.genres.items.rock') }}</div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Reggaeton.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">
-      {{ $t('PreviewPage.genres.items.reggaeton') }}
-    </div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Indie.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">{{ $t('PreviewPage.genres.items.indie') }}</div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Classical.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">
-      {{ $t('PreviewPage.genres.items.classical') }}
-    </div>
-  </div>
-  <div class="genres__item">
-    <img class="genres__image" src="@/assets/images/preview/genres/Techno.jpg" />
-    <div class="genres__color__fade"></div>
-    <div class="genres__name">{{ $t('PreviewPage.genres.items.techno') }}</div>
-  </div>
-</div>
-</section>
-
-
 <template>
   <section class="genre__category">
-    <div class="genre__category__title">{{ $t('GenreCategory.title') }}</div>
-    <div class="genre__show__all"></div>
+    <div class="d-flex">
+      <div class="genre__category__title">{{ $t('GenreCategory.title') }}</div>
+      <router-link to="#" class="genre__show__all">Показать всё</router-link>
+    </div>
 
-    <GenreItem v-for="item in items"
-               :key="item.name"
-               :name="item.name"
-               :path="item.path" />
+    <div class="genres__list">
+      <GenreItem v-for="item in items"
+                 :key="item.name"
+                 :name="item.name"
+                 :path="item.path" />
+    </div>
   </section>
 </template>
 
 <script>
 import GenreItem from '@/components/Main/genreCategory/GenreItem.vue'
+import { useI18n } from 'vue-i18n'
 
 export default {
   name: 'GenreCategory',
   components: { GenreItem },
   setup() {
+    const { t } = useI18n()
     const items = [
       {
-        name: 'GenreItem',
-        path: '/genre-category/GenreItem'
+        name: t('PreviewPage.genres.items.hiphop'),
+        path: new URL('@/assets/images/preview/genres/hiphop.jpg', import.meta.url).href
       },
       {
-        name: 'GenreItem',
-        path: '/genre-category/GenreItem'
+        name: t('PreviewPage.genres.items.hiphop'),
+        path: new URL('@/assets/images/preview/genres/genreitem.jpg', import.meta.url).href // замените путь на реальный путь к изображению
       }
     ]
     return {
@@ -90,13 +43,7 @@ export default {
 <style lang="scss">
 .genre__category {
   width: 100%;
-  background-color: red;
-}
-
-.genre-category__item {
-}
-
-.genre-category__button {
-  /* Стили по методологии БЭМ */
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- frontend/public/src/src/components/Main/genreCategory/GenreItem.vue
+++ frontend/public/src/src/components/Main/genreCategory/GenreItem.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>{{ name }}</div>
+  <router-link to="#" class="genres__item">
+    <img class="genres__image"
+         :src="path" :alt="path" />
+    <div class="genres__color__fade"></div>
+    <div class="genres__name">{{ name }}</div>
+  </router-link>
+
 </template>
 
 <script>
 export default {
   name: 'GenreItem',
   props: {
-    name: '',
-    path: ''
+    name: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: String,
+      required: true
+    }
   },
   setup() {
 
